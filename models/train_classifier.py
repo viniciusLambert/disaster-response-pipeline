74488f72633838758d34ed5db30cdb2f768052a0
viniciusLambert <huevinne@gmail.com>
2022-02-04 14:39:21 -0300

    complete ML notebook

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,96 @@
 import sys
+import pandas as pd
+import numpy as np
+import pickle
 
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sqlalchemy import create_engine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+
+from sklearn.metrics import classification_report
 
 def load_data(database_filepath):
-    pass
+    ngine = create_engine(f'sqlite:///{database_filepath}')
+    df =  pd.read_sql_query ( "SELECT * FROM LabeledMessages", engine)
+
+    X = df["message"]
+    y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    return X, y
 
 
 def tokenize(text):
-    pass
+    norm_text = text.lower()
+    tokens = word_tokenize(norm_text)
+    lemmatizer = WordNetLemmatizer()
 
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok)
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+            ('starting_verb', StartingVerbExtractor())
+        ])),
+
+        ('lr_multi', MultiOutputClassifier(
+            RandomForestClassifier())
+        )
+    ])
+    
+    
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
 
+    y_pred = model.predict(X_test)
+
+    for column in range(y_pred.shape[1]):
+        print("Column: ",category_names[column])
+        print(classification_report(y_test.to_numpy()[:,column], y_pred[:,column]))
+        print("----------------------------------------------------------*")
+    print("\nBest Parameters:", cv.best_params_)
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open( model_filepath, 'wb'))
 
 
 def main():
