4a00742281224156110f444710a8af4713455ef6
viniciusLambert <huevinne@gmail.com>
2022-02-10 15:35:34 -0300

    update: test new heroku deploy

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import imp
-from lib2to3.pgen2.tokenize import tokenize
-from app.run import app
-from app.run import tokenize
-
-if __name__ == "__main__":
-  #app.run(host='0.0.0.0', port=3001, debug=True)
-  app.run()
